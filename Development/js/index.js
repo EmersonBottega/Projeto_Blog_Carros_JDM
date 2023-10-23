4fb528bd50219afc675b562038484b675db067f7
Emerson Bottega da Silva <emerbtts@hotmail.com>
2023-10-23 15:41:57 -0300

    add: styling & fixing header

--- Development/js/index.js
+++ Development/js/index.js
@@ -1,29 +1,28 @@
 function addComment() {
-    let commentInput = document.getElementById("comment-input");
-    let commentText = commentInput.value;
+    const commentInput = document.getElementById("comment-input");
+    const commentText = commentInput.value;
+
     if (commentText.trim() === "") {
         alert("Please enter a comment.");
         return;
     }
 
-    let commentList = document.getElementById("comment-list");
+    const commentList = document.getElementById("comment-list");
     let newComment = document.createElement("li");
     newComment.innerHTML = commentText + ' <button class="delete-btn" onclick="deleteComment(this)">Delete</button>';
     commentList.appendChild(newComment);
-
     newComment.classList.add("comment-item");
-
     commentInput.value = "";
 }
 
 function deleteComment(commentElement) {
-    let commentList = document.getElementById("comment-list");
+    const commentList = document.getElementById("comment-list");
     commentList.removeChild(commentElement.parentNode);
 }
 
 function validarEmail() {
-    var emailInput = document.getElementById("email");
-    var email = emailInput.value;
+    const emailInput = document.getElementById("email");
+    const email = emailInput.value;
 
     if (email.includes("@")) {
         alert("Email registered successfully!");
